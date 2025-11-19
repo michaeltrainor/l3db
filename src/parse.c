9ea4a57abe0017db975e61872d61c51bd6ee8d14
mtd3v <accounts@michaeltrainor.com>
2025-11-19 12:26:52 -0800

    moving from wsl to a container

---
+++ src/parse.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "common.h"
+#include "parse.h"
+
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    
+    if (header == NULL) {
+        printf("calloc failed to allocate memory for a dbheader_t.\n");
+        return STATUS_ERROR;
+    }
+
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
+
+int validate_db_header(int fd, struct dbheader_t **headerOut) {
+
+}
+
+int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut) {
+
+}
